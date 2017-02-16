6f25fef1b2deeffb359c16026920dbfa42d5eb71
Ricardo Sanchez <Ricardo_Sanchez@epam.com>
2017-02-16 16:50:10 -0600

    Post new challenge

--- src/containers/challenge/ChallengeView.js
+++ src/containers/challenge/ChallengeView.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Users from './Users';
-import { Modal, Button, Row, Col, Input } from 'react-materialize';
+import { Modal, Button, Row, Col, Input, Preloader } from 'react-materialize';
 import Link from 'react-router';
-import { firebaseFetch } from '../../services/firebaseService';
+import { firebaseFetch, firebasePost } from '../../services/firebaseService';
 import './challenge.css';
 
 class ChallengeView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      disciplines: []
+      disciplines: [],
+      challengedUser: '',
+      selectedDiscipline: '',
+      comment: '',
+      isLoading: false,
+      user:''
     }
   }
 
@@ -23,20 +28,45 @@ class ChallengeView extends Component {
       })
   }
 
+  handleSelectedUser(userKey) {
+    this.setState({ challengedUser: userKey })
+  }
+
+  onBlurComment(event) {
+    this.setState({ comment: event.target.value });
+  }
+
+  onChangeSelect(event) {
+    this.setState({ selectedDiscipline: event.target.value });
+  }
+
+  sendChallenge() {
+    if (this.state.challengedUser !== '') {
+      this.setState({ isLoading: true });
+      firebasePost('challenges', {
+        data: {
+          sender: this.state.user,
+          receiver: this.state.challengedUser,
+          status: 1,
+          comment: this.state.comment,
+          discipline: this.state.selectedDiscipline
+        }
+      }).then(() => this.setState({ isLoading: false }))
+    } else {
+      console.log('mensaje de error');
+    }
+  }
+
   render() {
-    return (
-      <Modal
-        header='Select Match'
-        trigger={
-          <Button waves='light'>Create Challenge</Button>
-        }
-        actions={
-          [
-            <Button waves='light' modal='close' flat>Close</Button>,
-            <Button waves='light' className="teal" >Send Challenge</Button>
-          ]
-        }
-        className="modal">
+    let layout;
+    if (this.state.isLoading) {      
+      layout = (
+        <Col s={12} className="loader">
+          <Preloader flashing size='big' />
+        </Col>
+      )
+    } else {
+      layout = (
         <Row className="row-container">
           <Row>
             <Col s={5} className="user-container" >
@@ -59,12 +89,12 @@ class ChallengeView extends Component {
               </div>
             </Col>
             <Col s={5} className="user-container">
-              <Users user={this.props.user} />
+              <Users handleSelectedUser={this.handleSelectedUser.bind(this)} user={this.props.user} />
             </Col>
           </Row>
           <Row>
             <Col m={6} className="txt-center offset-m3">
-              <Input m={12} type='select' label="Challenge" defaultValue='0'>
+              <Input m={12} type='select' label="Challenge" defaultValue='0' onChange={this.onChangeSelect.bind(this)}>
                 {
                   this.state.disciplines.map((discipline, i) => {
                     return <option value={i} key={i}>{discipline}</option>
@@ -73,11 +103,28 @@ class ChallengeView extends Component {
               </Input>
               <Col m={12} className="input-field">
                 <label className="">Comment</label>
-                <textarea id="comment" className="materialize-textarea"></textarea>                
-              </Col>              
+                <textarea id="comment" className="materialize-textarea" onBlur={this.onBlurComment.bind(this)}></textarea>
+              </Col>
             </Col>
           </Row>
         </Row>
+      )
+    }
+
+    return (
+      <Modal
+        header='Select Match'
+        trigger={
+          <Button waves='light'>Create Challenge</Button>
+        }
+        actions={
+          [
+            <Button waves='light' modal='close' flat>Close</Button>,
+            <Button waves='light' className="teal" onClick={this.sendChallenge.bind(this)}>Send Challenge</Button>
+          ]
+        }
+        className="modal">        
+          {layout}        
       </Modal>
     );
   }
